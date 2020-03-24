0fb2884ecc239901da5b5e7cb181158c752db55d
Angeless818 <59379680+Angeless818@users.noreply.github.com>
2020-03-24 23:38:09 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-define('APIKEY', 'AIzaSyABMsmVIClxOSLvisIciSzfDqzBmnUt_WE');
+define('APIKEY', 'AIzaSyA3fOsUktQ_IvosdU_GhcidvxJ0CCfswUo');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'Angeless youtube');
-define('TITLENAME', 'Angeless youtube');
-define('EN2DEKEY', '32123wwerwertgfdgds');
+define('SITE_NAME', 'Angeless');
+define('TITLENAME', 'U2 mirror');
+define('EN2DEKEY', 'LAFX4xO5RC');
 define('EMAIL', 'malishyo@gmail.com');
-define('NAME', 'DarkAngel');
+define('NAME', 'Angeless');
 define('PASSWORD', '@ss6nss74');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
